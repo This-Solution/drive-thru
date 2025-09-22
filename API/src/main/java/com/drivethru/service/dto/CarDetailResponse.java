b761e5faed25a2fafa8702806a0943a9a54fbfd0
Harshid-Patel <Harshid@radicalrack.com>
2025-09-22 15:52:18 +0530

    change carDetails api

--- API/src/main/java/com/drivethru/service/dto/CarDetailResponse.java
+++ API/src/main/java/com/drivethru/service/dto/CarDetailResponse.java
@@ -4,26 +4,91 @@ import java.util.List;
 
 public class CarDetailResponse {
     private Integer carId;
-    private List<String> imageUrl;
     private String carPlateNumber;
-    private Long orderCount;
+    private String carType;
+    private String carImageUrl;
+    private String plateImageUrl;
     private String carColor;
-    private String carColorStatus;
 
-    public Long getOrderCount() {
-        return orderCount;
+    private long last30DayCount;
+    private long last30To60DayCount;
+    private long last60To90DayCount;
+
+    private String last30DayColorStatus;
+    private String last30To60DayColorStatus;
+    private String last60To90DayColorStatus;
+
+
+    public long getLast30DayCount() {
+        return last30DayCount;
     }
 
-    public void setOrderCount(Long orderCount) {
-        this.orderCount = orderCount;
+    public void setLast30DayCount(long last30DayCount) {
+        this.last30DayCount = last30DayCount;
     }
 
-    public List<String> getImageUrl() {
-        return imageUrl;
+    public long getLast30To60DayCount() {
+        return last30To60DayCount;
     }
 
-    public void setImageUrl(List<String> imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setLast30To60DayCount(long last30To60DayCount) {
+        this.last30To60DayCount = last30To60DayCount;
+    }
+
+    public long getLast60To90DayCount() {
+        return last60To90DayCount;
+    }
+
+    public void setLast60To90DayCount(long last60To90DayCount) {
+        this.last60To90DayCount = last60To90DayCount;
+    }
+
+    public String getLast30DayColorStatus() {
+        return last30DayColorStatus;
+    }
+
+    public void setLast30DayColorStatus(String last30DayColorStatus) {
+        this.last30DayColorStatus = last30DayColorStatus;
+    }
+
+    public String getLast30To60DayColorStatus() {
+        return last30To60DayColorStatus;
+    }
+
+    public void setLast30To60DayColorStatus(String last30To60DayColorStatus) {
+        this.last30To60DayColorStatus = last30To60DayColorStatus;
+    }
+
+    public String getLast60To90DayColorStatus() {
+        return last60To90DayColorStatus;
+    }
+
+    public void setLast60To90DayColorStatus(String last60To90DayColorStatus) {
+        this.last60To90DayColorStatus = last60To90DayColorStatus;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getCarImageUrl() {
+        return carImageUrl;
+    }
+
+    public void setCarImageUrl(String carImageUrl) {
+        this.carImageUrl = carImageUrl;
+    }
+
+    public String getPlateImageUrl() {
+        return plateImageUrl;
+    }
+
+    public void setPlateImageUrl(String plateImageUrl) {
+        this.plateImageUrl = plateImageUrl;
     }
 
     public Integer getCarId() {
@@ -50,11 +115,4 @@ public class CarDetailResponse {
         this.carColor = carColor;
     }
 
-    public String getCarColorStatus() {
-        return carColorStatus;
-    }
-
-    public void setCarColorStatus(String carColorStatus) {
-        this.carColorStatus = carColorStatus;
-    }
 }
