e2581082ddb90679d78a51559c7038a38298c7f6
javiya_125 <harshad@radicalrack.com>
2025-09-24 19:17:36 +0530

    changes

--- UI/src/pages/Orders/orderManage.js
+++ UI/src/pages/Orders/orderManage.js
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import Loader from 'components/Loader';
 import OrderCard from 'components/OrderCard';
 import { useSelector } from 'store';
 
@@ -7,15 +8,17 @@ const OrderManage = () => {
   return (
     <>
       <Grid container display={'flex'} direction={'row'} spacing={2}>
-        {cameraConfig &&
-          cameraConfig.length > 0 &&
+        {cameraConfig && cameraConfig.length > 0 ? (
           cameraConfig.map((camera, index) => {
             return (
               <Grid item xs={12} md={12 / cameraConfig.length} key={index}>
                 <OrderCard cameraConfig={camera} />
               </Grid>
             );
-          })}
+          })
+        ) : (
+          <Loader />
+        )}
       </Grid>
     </>
   );
