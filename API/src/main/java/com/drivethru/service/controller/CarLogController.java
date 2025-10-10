2733c274fa8f33160e14cc46b2dacd19475f12ea
Harshid-Patel <Harshid@radicalrack.com>
2025-10-10 12:10:49 +0530

    Update CarLogController.java

--- API/src/main/java/com/drivethru/service/controller/CarLogController.java
+++ API/src/main/java/com/drivethru/service/controller/CarLogController.java
@@ -7,10 +7,7 @@ import com.drivethru.service.service.CarLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/CarLog")
@@ -19,7 +16,7 @@ public class CarLogController {
     @Autowired
     CarLogService carLogService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<ResponseObject<CarLog>> getCarDetail(@RequestBody CarDetailRequest carDetailRequest) {
         ResponseObject<CarLog> responseObject = new ResponseObject<>();
         CarLog carLog = carLogService.getCarLogDetails(carDetailRequest);
